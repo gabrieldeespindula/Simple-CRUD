38079b7986821344eda6523bc9e24a507f6e3f91
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-07-30 13:47:47 -0300

    Mapping interface elements.

--- app/src/main/java/com/gabrieldeespindula/simplecrud/Edit.java
+++ app/src/main/java/com/gabrieldeespindula/simplecrud/Edit.java
@@ -2,12 +2,25 @@ package com.gabrieldeespindula.simplecrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Edit extends AppCompatActivity {
+    EditText bookTitle;
+    EditText bookAuthor;
+    EditText bookPublishingCompany;
+    Button buttonSave;
+    Button buttonDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        bookTitle = findViewById(R.id.editTextBookTitle);
+        bookAuthor = findViewById(R.id.editTextBookAuthor);
+        bookPublishingCompany = findViewById(R.id.editTextBookPublishingCompany);
+        buttonSave = findViewById(R.id.buttonSave);
+        buttonDelete = findViewById(R.id.buttonDelete);
     }
 }
