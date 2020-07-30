f3df55fa54a28e51220796f7c6007d7225ea0f98
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-07-30 14:09:03 -0300

    Creating static values.

--- app/src/main/java/com/gabrieldeespindula/simplecrud/CreateDB.java
+++ app/src/main/java/com/gabrieldeespindula/simplecrud/CreateDB.java
@@ -6,6 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
 public class CreateDB extends SQLiteOpenHelper {
+    private static final String NAME_DATABASE = "DataBase.db";
+    private static final String TABLE_NAME = "Books";
+    private static final String ID = "_id";
+    private static final String TITLE = "Title";
+    private static final String AUTHOR = "Author";
+    private static final String COMPANY = "Company";
+    private static final int VERSION = 1;
 
     public CreateDB(Context context, @Nullable @Nullable String name, @Nullable @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
