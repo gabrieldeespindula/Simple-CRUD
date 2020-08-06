05372dee3ae0be068e404db4740e114f9872e16b
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-08-06 13:46:14 -0300

    Implementing SimpleCursorAdapter, now you can read.

--- app/src/main/java/com/gabrieldeespindula/simplecrud/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/simplecrud/MainActivity.java
@@ -1,12 +1,17 @@
 package com.gabrieldeespindula.simplecrud;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         FloatingActionButton newActivity =  findViewById(R.id.floatingActionButton);
         newActivity.setOnClickListener(this);
+
+        DBController crud = new DBController(getBaseContext());
+        Cursor cursor = crud.loadingDB();
+
+        String[] nameSpaces = new String[] {CreateDB.ID, CreateDB.TITLE};
+        int[] idViews = new int[] {R.id.idBook, R.id.nameBook};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getBaseContext(),
+                R.layout.gridlayout,cursor,nameSpaces,idViews, 0);
+        list = (ListView)findViewById(R.id.listView);
+        list.setAdapter(adapter);
     }
 
     @Override
